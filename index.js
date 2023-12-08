f3bee38f250bfc9ded91e41707fcc72e65a68b1e
ayushsingh3007 <ayushsingh6394023007@gmail.com>
2023-12-08 01:08:44 -0800

    new updatinon is done

--- index.js
+++ index.js
@@ -9,17 +9,13 @@ const PORT = process.env.PORT || 4000;
 const authUser = require('./routes/authUser');
 const bodyParser = require('body-parser');
 const { notfound, errorHandler } = require('./middlewares/errorHandler');
-const { data } = require('./routes/data');
+
 const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
 dbConnect();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api/user', (req, res) => {
-  const jsonData = JSON.stringify(data);
-  
-  res.send(jsonData);
-});
+
 app.use('/api/user', authUser);
 app.use(notfound);
 app.use(errorHandler);
