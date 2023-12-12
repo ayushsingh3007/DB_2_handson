1566a31bf713b07ca7cc63c4ffcddf42088d995d
ayushsingh3007 <ayushsingh6394023007@gmail.com>
2023-12-12 02:12:43 -0800

    dfsf

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,13 +1,11 @@
-const mongoose = require('mongoose');
 
-const dbConnect = () => {
-    mongoose.connect(process.env.Mongourl, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log('MongoDB connected successfully');
-        })
-        .catch((err) => {
-            console.error('MongoDB connection error:', err.message);
-        });
-};
-
-module.exports = dbConnect;
+const {default:mongoose}=require('mongoose')
+const dbConnect=()=>{
+    try{
+        const conn=mongoose.connect(process.env.Mongourl)
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+module.exports=dbConnect;
