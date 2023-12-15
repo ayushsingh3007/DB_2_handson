05bae0bd3a1f9bb140a92e38f36cc6cbb9856e45
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-12-15 02:40:18 -0800

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -90,8 +90,8 @@ const payment= async (req, res) => {
     payment_method_types:["card"],
     line_items:lineItems,
     mode:"payment",
-    success_url:"http://localhost:3000/",
-    cancel_url:"http://localhost:3000/cart",
+    success_url:"https://ecommerce-website-ruddy.vercel.app/shop",
+    cancel_url:"https://ecommerce-website-ruddy.vercel.app/shop",
   });
   res.json({id:session.id})
 };
